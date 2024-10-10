b85ac9569e2da87bf7ff6f2e29072bb1e3090361
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2024-10-09 21:59:47 -0300

    chore: produtos
    - links para aba produtos
    - admin produtos
    - url
    - view

--- core/context_processors.py
+++ core/context_processors.py
@@ -15,6 +15,44 @@ def menu_items(request):
             "permission": "vendas.view_loja",
             "section": "Dr Smart",
         },
+        {
+            "label": "Produtos",
+            "icon": "bx bx-home-circle",
+            "permission": "produtos.view_produto",
+            "section": "Dr Smart",
+            "sub_items": [
+                {
+                    "label": "Produtos",
+                    "url_name": "produtos:produtos",
+                    "permission": "produtos.view_produto",
+                },
+                {
+                    "label": "Cor",
+                    "url_name": "produtos:produtos",
+                    "permission": "produtos.view_produto",
+                },
+                {
+                    "label": "Estado",
+                    "url_name": "produtos:produtos",
+                    "permission": "produtos.view_produto",
+                },
+                {
+                    "label": "Fabricante",
+                    "url_name": "produtos:produtos",
+                    "permission": "produtos.view_produto",
+                },
+                {
+                    "label": "Memoria",
+                    "url_name": "produtos:produtos",
+                    "permission": "produtos.view_produto",
+                },
+                {
+                    "label": "Tipo",
+                    "url_name": "produtos:produtos",
+                    "permission": "produtos.view_produto",
+                },
+            ]
+        },
         # {
         #     "label": "Exemplo",
         #     "icon": "bx bx-dollar-circle",
--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from produtos.models import Produto
+
+class ProdutoListView(ListView):
+    model = Produto
+    template_name = 'produtos.html'
+    context_object_name = 'produtos'
