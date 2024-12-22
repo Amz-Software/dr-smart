833ed74ecf6807f29d3da6c42ff6834f1a9fce99
Thiago Menezes <thimenezes19@gmail.com>
2024-12-22 11:01:36 -0300

    fix whitenoise

--- core/settings.py
+++ core/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
 
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
