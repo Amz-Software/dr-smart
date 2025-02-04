0e4daa9b35bab8dcbe5b977401517e98ec8d5083
Pedro Henrique <93086323+pedrohocc@users.noreply.github.com>
2025-02-04 18:31:06 -0300

    fix: valor folha caixa

--- vendas/views.py
+++ vendas/views.py
@@ -599,6 +599,7 @@ class FolhaCaixaPDFView(PermissionRequiredMixin, View):
 
         caixa_valor_final = (caixa.saldo_total_dinheiro + caixa.entradas) - caixa.saidas
         valor_final = entrada_total - saida_total
+        valor_por_tipo_pagamento_total = sum(valor_venda_por_tipo_pagamento.values())
 
         context = {
             'caixa': caixa,
@@ -607,6 +608,7 @@ class FolhaCaixaPDFView(PermissionRequiredMixin, View):
             'lancamentos': lancamentos,
             'entrada_total': entrada_total,
             'saida_total': saida_total,
+            'valor_por_tipo_pagamento_total': valor_por_tipo_pagamento_total,
             'saldo_total': saldo_total,
             'valor_venda_por_tipo_pagamento': valor_venda_por_tipo_pagamento.items(),
             'caixa_valor_final': caixa_valor_final,
